646de77e764f1f4cff3a3505ee4956701f4607f3
Diego Tarrio <Diegootarrio@gmail.com>
2024-03-12 17:51:25 -0300

    primera entrega

---
+++ main.py
@@ -0,0 +1,100 @@
+import requests
+import pandas as pd
+import json
+import psycopg2
+
+
+
+# Definir la URL del endpoint de la API de CoinGecko
+url = 'https://api.coingecko.com/api/v3/coins/markets'
+
+# Definir Ids de monedas a extraer
+Ids_moneda= 'bitcoin,ethereum,tether,binancecoin,solana,staked-ether,ripple,usd-coin,cardano,dogecoin,shiba-inu'
+
+# Parámetros de la solicitud
+params = {
+    'vs_currency': 'usd',  # Moneda de referencia (en este caso, dólares estadounidenses)
+    'order': 'market_cap_desc',  # Ordenar por capitalización de mercado descendente
+    'per_page': 100,  # Número de resultados por página (en este caso, 100)
+    'page': 1,  # Página de resultados (en este caso, la primera página)
+    'ids': Ids_moneda
+
+}
+
+# Realizar la solicitud a la API
+response = requests.get(url,params=params)
+
+# Verificar si la solicitud fue exitosa (código de estado 200)
+if response.status_code == 200:
+    # Obtener los datos de la respuesta en formato JSON
+    data = response.json()
+    # Crear un DataFrame de pandas a partir de los datos
+    df = pd.DataFrame(data)
+   
+else:
+    # Si la solicitud no fue exitosa, imprimir el código de estado
+    print('Error:', response.status_code)
+    print('response text: ', response.text)
+
+
+with open('credentials.json') as f:
+    credentials = json.load(f)
+    dbname = credentials['dbname']
+    host = credentials['host']
+    port = credentials['port']
+    user = credentials['user']
+    password = credentials['password']
+
+
+# Establecer la conexión con la base de datos Redshift
+conn = psycopg2.connect(
+    dbname = dbname,
+    user = user,
+    password = password,
+    host = host,
+    port = port
+)
+
+# Crear un cursor para ejecutar consultas
+cur = conn.cursor()
+
+# Definir la consulta SQL para crear la tabla
+create_table_query = """
+CREATE TABLE coins_data (
+    ID VARCHAR(255) PRIMARY KEY,
+    Symbol VARCHAR(255),
+    Name VARCHAR(255),
+    Image VARCHAR(255),
+    Current_Price DECIMAL,
+    Market_Cap DECIMAL,
+    Market_Cap_Rank INT,
+    Fully_Diluted_Valuation DECIMAL,
+    Total_Volume DECIMAL,
+    High_24h DECIMAL,
+    Low_24h DECIMAL,
+    Price_Change_24h DECIMAL,
+    Price_Change_Percentage_24h DECIMAL,
+    Market_Cap_Change_24h DECIMAL,
+    Market_Cap_Change_Percentage_24h DECIMAL,
+    Circulating_Supply DECIMAL,
+    Total_Supply DECIMAL,
+    Max_Supply DECIMAL,
+    Ath DECIMAL,
+    Ath_Change_Percentage DECIMAL,
+    Ath_Date DATE,
+    Atl DECIMAL,
+    Atl_Change_Percentage DECIMAL,
+    Atl_Date DATE,
+    Roi DECIMAL,
+    Last_Updated TIMESTAMP
+);
+"""
+
+
+# Ejecutar la consulta para crear la tabla
+cur.execute(create_table_query)
+
+# Confirmar la transacción y cerrar la conexión
+conn.commit()
+cur.close()
+conn.close()
